0ddd37d2823f6abdcb32dd944820aa08799c9862
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-05-11 19:40:03 +0530

    Modified number validation schema

--- examwebsite/src/schemas/index.js
+++ examwebsite/src/schemas/index.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export const userSchema = yup.object().shape({
-  rollno: yup.number().required("Required"),
+  rollno:yup.number().typeError("Roll number must be a valid number").positive("You must enter a positive number").integer("You must enter an integer"),
   dob: yup.date().required("Date is required").nullable()
 });
 
@@ -9,7 +9,7 @@ const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,}$/;
 // min 5 characters, 1 upper case letter, 1 lower case letter, 1 numeric digit.
 
 export const adminSchema = yup.object().shape({
-  idno: yup.number().required("Required"),
+  idno: yup.string().required("Required"),
   password: yup
     .string()
     .min(5)
